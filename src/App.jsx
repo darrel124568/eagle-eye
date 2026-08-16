22b8e14e4f9fe7139d6c3b32963957713f1dbf0e
Darrel <darrel124568@gmail.com>
2026-08-16 17:30:36 +0300

    Refactor App component to use lazy loading for routes and improve loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import BirdDetails from './pages/BirdDetails/BirdDetails'
-import Explorer from './pages/Explorer/Explorer'
-import Favourites from './pages/Favorites/Favorites'
-import LocalRadar from './pages/LocalRadar/LocalRadar'
-import Notes from './pages/notes/Notes'
+import { lazy, Suspense } from 'react'
+import Loading from './components/Loading/Loading'
 import {BirdProvider} from './context/birdContext'
 import {FavoritesProvider} from './context/favoritesContext'
 import {FeaturedBirdProvider} from './context/featuredBirdContext'
@@ -13,22 +8,31 @@ import {DetailedBirdProvider} from './context/detailedBirdContext'
 
 
 export default function App() {
+  const Home = lazy(() => import("./pages/Home/Home"));
+  const Explorer = lazy(() => import("./pages/Explorer/Explorer"));
+  const BirdDetails = lazy(() => import("./pages/BirdDetails/BirdDetails"));
+  const About = lazy(() => import("./pages/About/About"));
+  const Favorites = lazy(() => import("./pages/Favorites/Favorites"));
+  const Notes = lazy(() => import('./pages/notes/Notes'));
+  const LocalRadar = lazy(() => import('./pages/LocalRadar/LocalRadar'));
   return (
     <BirdProvider>
     <FavoritesProvider>
     <FeaturedBirdProvider>
     <DetailedBirdProvider>
     <BrowserRouter>
-    <Routes>
+    <Suspense fallback={<Loading/>}>
+    <Routes>      
       <Route path='/' element={<Home/>}/>
       <Route path='/explore' element={<Explorer/>}/>
       <Route path='/bird/:sciName' element={<BirdDetails/>}/>
       <Route path='/about' element={<About/>}/>
-      <Route path='/favorites' element={<Favourites/>}/>
+      <Route path='/favorites' element={<Favorites/>}/>
       <Route path='/notes' element={<Notes/>}/>
-      <Route path='/local-radar' element={<LocalRadar/>}/>
+      <Route path='/local-radar' element={<LocalRadar/>}/>        
       <Route path='*' element={<h1 className="p-6 text-2xl font-bold text-red-600">404 - Page Not Found</h1>}/>
     </Routes>
+    </Suspense>    
     </BrowserRouter>
     </DetailedBirdProvider>
     </FeaturedBirdProvider>
